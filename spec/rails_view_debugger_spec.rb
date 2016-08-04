528bf4ccc1b372fa43462dab19bc575aa165822b
Gearnode <bfrimin@student.42.fr>
2016-08-04 17:49:05 +0200

    Add spec

--- spec/rails_view_debugger_spec.rb
+++ spec/rails_view_debugger_spec.rb
@@ -5,7 +5,32 @@ describe RailsViewDebugger do
     expect(RailsViewDebugger::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'when block is given' do
+
+    it 'calls logger 3 times' do
+      expect(RailsViewDebugger::LOGGER).to receive(:error).exactly(3).times
+      expect{ RailsViewDebugger.trace { raise 'hello' } }.to raise_error
+    end
+
+    context 'when block raise' do
+
+      it 'raises error' do
+        expect{ RailsViewDebugger.trace { raise 'error' } }.to raise_error(StandardError)
+      end
+    end
+
+    context 'when block raise nothing' do
+
+      it 'raises nothing' do
+        expect{ RailsViewDebugger.trace {} }.to_not raise_error
+      end
+    end
+  end
+
+  context 'when block is not given' do
+
+    it 'returns nil' do
+      expect(RailsViewDebugger.trace).to eq nil
+    end
   end
 end
